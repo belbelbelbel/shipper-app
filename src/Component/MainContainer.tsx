e67567519cc14d26f192d86c7edc12cb1bd2a105
belbelbelbel <gronaldchia@gmail.com>
2024-11-06 18:23:38 +0100

    mapping in progress

--- src/Component/MainContainer.tsx
+++ src/Component/MainContainer.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Outlet, useNavigate } from "react-router-dom"
-import { MdDashboard, MdYard } from "react-icons/md";
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
+import { MdDashboard } from "react-icons/md";
 import { IoIosPeople } from "react-icons/io";
 import { IoAccessibilitySharp, IoSettingsSharp } from "react-icons/io5";
 import { IoMdNotifications } from "react-icons/io";
 import { IoLogOut } from "react-icons/io5";
-import { RiLinksFill, RiNavigationFill } from "react-icons/ri";
+import { RiLinksFill } from "react-icons/ri";
+
 export const MainContainer = () => {
     const BusinessName = localStorage.getItem('selectedRoleId')
     const navigate = useNavigate()
-    const profilename: any = localStorage.getItem('profilename')
+    const location = useLocation()
+    const profilename = localStorage.getItem('profilename') || '';
+  
     const LinkArray = [
         {
             icon: <MdDashboard size={22} />,
@@ -17,11 +20,10 @@ export const MainContainer = () => {
             name: 'Dashboard',
         },
         {
-            icon: <RiLinksFill  size={25} fontWeight={'bold'}/>,
+            icon: <RiLinksFill size={25} fontWeight={'bold'}/>,
             path: 'overview/connections',
             name: 'Connections'
         },
-       
         {
             icon: <IoIosPeople size={25} />,
             path: 'overview/suppliers',
@@ -42,35 +44,37 @@ export const MainContainer = () => {
             path: 'overview/inventory',
             name: 'Inventory'
         }
-     
     ]
 
-    const capitalizeFirstLetter = (string: string) => {
+    const capitalizeFirstLetter = (string:string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
 
-    const firstLetter = (value: string) => {
+    const firstLetter = (value:string) => {
         return value.charAt(0).toUpperCase();
     }
     const capitalizedText = capitalizeFirstLetter(profilename);
 
     const firstLetterText = firstLetter(profilename);
 
-
     return (
         <div className='w-screen bg-blue-100 bg-opacity-[0.3] h-screen'>
-            <div className='flex w-full  h-[100%] overflow-hidden'>
-                <div className='w-[20%] py-4 flex flex-col justify-between  shadow-xl bg-white h-full'>
-                    <div className='flex flex-col gap-3 text-white   items-center h-full   '>
-                        <div className='self-start flex w-[70%] mx-auto '>
+            <div className='flex w-full h-[100%] overflow-hidden'>
+                <div className='w-[20%] py-4 flex flex-col justify-between shadow-xl bg-white h-full'>
+                    <div className='flex flex-col gap-3 text-white items-center h-full'>
+                        <div className='self-start flex w-[70%] mx-auto'>
                             <div className='text-[2.2rem] text-black hero-logo tracking-[1px]'>Shi-Go</div>
                         </div>
                         {LinkArray.map(link => (
-                            <div key={link.path} className='w-[12rem] mt-4 h-7 rounded-[5px] text-black cursor-pointer '>
-                                <a href={`/${link.path}`} className=''>
-                                    <div className='flex items-center  gap-4'>
-                                        <div > {link.icon}</div>
-                                        <div className='text-black'>{link.name}</div>
+                            <div 
+                                key={link.path} 
+                                className={`w-[12rem] mt-4 h-7 rounded-[5px] text-black cursor-pointer 
+                                    ${location.pathname.includes(link.path) ? 'text-red-500 font-semibold' : 'text-gray-700'}`}
+                            >
+                                <a href={`/${link.path}`}>
+                                    <div className='flex items-center gap-4'>
+                                        <div>{link.icon}</div>
+                                        <div>{link.name}</div>
                                     </div>
                                 </a>
                             </div>
@@ -82,16 +86,19 @@ export const MainContainer = () => {
                     </div>
                 </div>
                 <div className='h-full pb-20 w-[80%]'>
-                    <div className='w-full items-center pl-0 flex justify-end h-20 bg-white  shadow-md'>
+                    <div className='w-full items-center pl-0 flex justify-end h-20 bg-white shadow-md'>
                         <div className='items-center justify-center flex w-full'>
-                            <input type="text" className='border-[0.8px] border-black w-[80%] text-[1.05rem] mx-auto px-4 py-2 outline-0 shadow-md rounded-[10px]' placeholder='e.g agricultural aspect' />
+                            <input 
+                                type="text" 
+                                className='border-[0.8px] border-black w-[80%] text-[1.05rem] mx-auto px-4 py-2 outline-0 shadow-md rounded-[10px]' 
+                                placeholder='e.g agricultural aspect' 
+                            />
                         </div>
                         <div className='cursor-pointer w-[30%] gap-3 flex items-center'>
-                            {/* <img src="/src/assets/profilepic.png" alt="profile" className='w-12 h-12' /> */}
                             <div className='w-[4rem] items-center justify-center flex h-[3rem] bg-blue-100 bg-opacity-[1] shadow rounded-[100%]'>
                                 <div className='font-bold'>{firstLetterText}</div>
                             </div>
-                            <div className='flex w-full  flex-col'>
+                            <div className='flex w-full flex-col'>
                                 <div className='text-[1rem] font-bold'>{capitalizedText}</div>
                                 <div>{BusinessName === '1' ? "Business Owner" : 'Supplier'}</div>
                             </div>
@@ -103,5 +110,6 @@ export const MainContainer = () => {
         </div>
     )
 }
+
 
 // when i click on each suplier i can access the descrition of the suppler and the location on the map  and then i can know the latitude and the longitude of the displacement on total order
